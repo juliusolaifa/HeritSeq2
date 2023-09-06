271e17761dd91de324b3eca2732e5c86db6e78cb
juliusolaifa <jbolaifa@gmail.com>
2023-09-05 21:29:29 -0500

    data too large

---
+++ R/LXSmRNA.R
@@ -0,0 +1,18 @@
+#' LXSmRNA Matrix
+#'
+#' A matrix containing expression levels (or relevant measures) of 14,184 features across 232 samples.
+#' These samples are classified into two groups: 'Saline' and 'Ethanol'. This dataset aims to capture
+#' the potential differential expression or behavior of these features under the two conditions.
+#'
+#' @format A matrix with 14,184 rows (features) and 232 columns (samples).
+#' @source Radcliffe, Richard A et al. (2020). “Systems genetics analysis of the LXS recombinant inbred mouse strains: Genetic and molecular insights into acute ethanol tolerance”
+#' @details
+#' The `LXSmRNA` matrix represents a comprehensive dataset capturing the responses or behavior
+#' of various molecular features (potentially genes or other RNA molecules) under two different conditions: Saline and Ethanol.
+#' Each row in the matrix corresponds to a distinct feature, and each column represents a unique sample.
+#' The values within the matrix could represent expression levels, counts, or other relevant measures.
+#'
+#' The dataset plays a pivotal role in understanding the comparative behavior of these features
+#' between the two groups and could potentially be used for differential expression analysis, clustering,
+#' or other relevant bioinformatics or statistical analyses.
+"LXSmRNA"
